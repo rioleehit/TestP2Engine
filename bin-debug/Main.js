c8e04a4e840ea0d533b45fc61f1cc5d8213ad0a3
otis-out <otis-out@MAS.ljgroup.co>
2018-04-25 15:12:39 +0800

    版本升级

--- bin-debug/Main.js
+++ bin-debug/Main.js
@@ -26,14 +26,27 @@
 //  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //////////////////////////////////////////////////////////////////////////////////////
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Main = (function (_super) {
     __extends(Main, _super);
     function Main() {
-        _super.call(this);
-        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+        var _this = _super.call(this) || this;
+        _this.addEventListener(egret.Event.ADDED_TO_STAGE, _this.onAddToStage, _this);
+        return _this;
     }
-    var d = __define,c=Main,p=c.prototype;
-    p.onAddToStage = function (event) {
+    Main.prototype.onAddToStage = function (event) {
         //设置加载进度界面
         //Config to load process interface
         this.loadingView = new LoadingUI();
@@ -47,7 +60,7 @@ var Main = (function (_super) {
      * 配置文件加载完成,开始预加载preload资源组。
      * configuration file loading is completed, start to pre-load the preload resource group
      */
-    p.onConfigComplete = function (event) {
+    Main.prototype.onConfigComplete = function (event) {
         RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
@@ -59,7 +72,7 @@ var Main = (function (_super) {
      * preload资源组加载完成
      * Preload resource group is loaded
      */
-    p.onResourceLoadComplete = function (event) {
+    Main.prototype.onResourceLoadComplete = function (event) {
         if (event.groupName == "preload") {
             this.stage.removeChild(this.loadingView);
             RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
@@ -73,14 +86,14 @@ var Main = (function (_super) {
      * 资源组加载出错
      *  The resource group loading failed
      */
-    p.onItemLoadError = function (event) {
+    Main.prototype.onItemLoadError = function (event) {
         console.warn("Url:" + event.resItem.url + " has failed to load");
     };
     /**
      * 资源组加载出错
      *  The resource group loading failed
      */
-    p.onResourceLoadError = function (event) {
+    Main.prototype.onResourceLoadError = function (event) {
         //TODO
         console.warn("Group:" + event.groupName + " has failed to load");
         //忽略加载失败的项目
@@ -91,7 +104,7 @@ var Main = (function (_super) {
      * preload资源组加载进度
      * Loading process of preload resource group
      */
-    p.onResourceProgress = function (event) {
+    Main.prototype.onResourceProgress = function (event) {
         if (event.groupName == "preload") {
             this.loadingView.setProgress(event.itemsLoaded, event.itemsTotal);
         }
@@ -100,7 +113,7 @@ var Main = (function (_super) {
      * 创建游戏场景
      * Create a game scene
      */
-    p.createGameScene = function () {
+    Main.prototype.createGameScene = function () {
         var sky = this.createBitmapByName("bg_jpg");
         this.addChild(sky);
         var stageW = this.stage.stageWidth;
@@ -152,7 +165,7 @@ var Main = (function (_super) {
      * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
      * Create a Bitmap object according to name keyword.As for the property of name please refer to the configuration file of resources/resource.json.
      */
-    p.createBitmapByName = function (name) {
+    Main.prototype.createBitmapByName = function (name) {
         var result = new egret.Bitmap();
         var texture = RES.getRes(name);
         result.texture = texture;
@@ -162,7 +175,7 @@ var Main = (function (_super) {
      * 描述文件加载成功，开始播放动画
      * Description file loading is successful, start to play the animation
      */
-    p.startAnimation = function (result) {
+    Main.prototype.startAnimation = function (result) {
         var self = this;
         var parser = new egret.HtmlTextParser();
         var textflowArr = [];
@@ -190,10 +203,10 @@ var Main = (function (_super) {
      * 切换描述内容
      * Switch to described content
      */
-    p.changeDescription = function (textfield, textFlow) {
+    Main.prototype.changeDescription = function (textfield, textFlow) {
         textfield.textFlow = textFlow;
     };
     return Main;
 }(egret.DisplayObjectContainer));
-egret.registerClass(Main,'Main');
+__reflect(Main.prototype, "Main");
 //# sourceMappingURL=Main.js.map
